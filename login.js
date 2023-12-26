8d4c3255cc04f9822d1695b7557331b775d00142
RohithRK <98517385+RohithRK58@users.noreply.github.com>
2023-12-27 00:52:05 +0530

    Update login.js
    just html code for rough copy

---
+++ login.js
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+</head>
+<body>
+    
+    <form action="#" class="form" id="form">
+        <div>
+            <h1>LOGIN</h1>
+        </div>
+        <div class="form-control">
+            <label for="Username">UserName</label>
+            <input type="text" id="username">
+            <i class="fa-solid fa-circle-check"></i>
+            <i class="fa-solid fa-exclamation"></i>
+            <small>Error Message</small>
+        </div>
+        <div class="form-control">
+            <label for="Email">Email</label>
+            <input type="email" id="email">
+            <i class="fa-solid fa-circle-check"></i>
+            <i class="fa-solid fa-exclamation"></i>
+            <small>Error Message</small>
+
+        </div>
+        <div class="form-control">
+            <label for="Email">Email</label>
+            <input type="email" id="email">
+            <i class="fa-solid fa-circle-check"></i>
+            <i class="fa-solid fa-exclamation"></i>
+            <small>Error Message</small>
+
+        </div>
+        <div class="form-control">
+            <label for="Password">Password</label>
+            <input type="password" id="pass1">
+            <i class="fa-solid fa-circle-check"></i>
+            <i class="fa-solid fa-exclamation"></i>
+            <small>Error Message</small>
+
+        </div>
+        <div class="form-control">
+            <label for="cpassword">Confirm Password</label>
+            <input type="password" id="pass2">
+            <i class="fa-solid fa-circle-check"></i>
+            <i class="fa-solid fa-exclamation"></i>
+            <small>Error Message</small>
+
+        </div>
+        <button>Login</button>
+        <a href="#">Forgot Password</a>
+
+    </form>
+</body>
+</html>
